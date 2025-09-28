44f286a8e1a595ab0f8e5c888589ac8fbadbe594
markeckles <2435965705@qq.com>
2025-09-28 11:33:13 +0800

    提交

--- color.go
+++ color.go
@@ -1,6 +1,12 @@
 package pcolor
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var shortLogger = log.New(os.Stdout, "", 0)
 
 func Color(color int, str string) string {
 	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, str)
@@ -23,4 +29,29 @@ func Succ(str string, a ...any) string {
 func Warn(str string, a ...any) string {
 	str = fmt.Sprintf(str, a...)
 	return Color(33, "⚠️ "+str)
+}
+
+func Fatal(str string, a ...any) string {
+	str = fmt.Sprintf(str, a...)
+	return Color(91, "💀 "+str)
+}
+
+func PrintError(err error) {
+	shortLogger.Println(Error(err))
+}
+
+func PrintErr(str string, a ...any) {
+	shortLogger.Println(Err(str, a))
+}
+
+func PrintSucc(str string, a ...any) {
+	shortLogger.Println(Succ(str, a))
+}
+
+func PrintWarn(str string, a ...any) {
+	shortLogger.Println(Warn(str, a))
+}
+
+func PrintFatal(str string, a ...any) {
+	shortLogger.Println(Fatal(str, a))
 }
